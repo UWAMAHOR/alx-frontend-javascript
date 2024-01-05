c6b128914bc9c0417ad5f708fc28e0a2b40ce34a
UWAMAHOR <uwamahoropascaline11@gmail.com>
2024-01-05 09:41:23 -0800

    updates

--- 0x02-ES6_classes/8-hbtn_class.js
+++ 0x02-ES6_classes/8-hbtn_class.js
@@ -1,18 +1,18 @@
 export default class HolbertonClass {
-  /* eslint-disable */
   constructor(size, location) {
+    if (typeof size !== 'number') {
+      throw new TypeError('Size must be a number');
+    } else if (typeof location !== 'string') {
+      throw new TypeError('Location must be a string');
+    }
+
     this._size = size;
     this._location = location;
   }
 
   [Symbol.toPrimitive](hint) {
-    // casting a string
-    if (hint === 'string') {
-      return this._location;
-    }
-
-    // casting a number
     if (hint === 'number') {
       return this._size;
     }
-    return
+    return this._location;
+  }
