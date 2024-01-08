44d71ac9f277117202a4ccb57a2ac6fa6b26c6b7
UWAMAHOR <uwamahoropascaline11@gmail.com>
2024-01-08 09:24:34 -0800

    ES6 PROMISES

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -1,12 +1,14 @@
-export default function getFullResponseFromApi(success) {
-  /* eslint-disable */
+export default function (boolean) {
   return new Promise((resolve, reject) => {
-    if (!success) {
-      reject(new Error('The fake API is not working currently'));
+    const object = {
+      status: 200,
+      body: 'Success',
+    };
+
+    if (boolean === true) {
+      resolve(object);
     } else {
-      resolve({
-	status: 200,
-	body: 'Success',
-      });
+      reject(Error('The fake API is not working currently'));
     }
   });
+}
